4c8d725f34cfe7b4689e8dbce44f6cc7e6a0fad9
Briqz23 <danielbriquez2@gmail.com>
2022-06-11 16:41:02 -0300

    segundo commit

--- src/main/java/com/mycompany/mavenproject1/EscolhaJogadorTela.java
+++ src/main/java/com/mycompany/mavenproject1/EscolhaJogadorTela.java
@@ -40,7 +40,6 @@ public class EscolhaJogadorTela extends javax.swing.JFrame {
         jLabel4 = new javax.swing.JLabel();
         jButton4 = new javax.swing.JButton();
         jButton1 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
 
         jogarU2Botao1.setText("Jogar");
@@ -76,7 +75,7 @@ public class EscolhaJogadorTela extends javax.swing.JFrame {
         jPanel3.setPreferredSize(new java.awt.Dimension(948, 592));
 
         jComboBoxNomesJogadores.setFont(new java.awt.Font("Montserrat Medium", 0, 24)); // NOI18N
-        jComboBoxNomesJogadores.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Admin" }));
+        jComboBoxNomesJogadores.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Admin", " " }));
         jComboBoxNomesJogadores.setBorder(null);
         jComboBoxNomesJogadores.setMaximumSize(new java.awt.Dimension(127, 200));
         jComboBoxNomesJogadores.addActionListener(new java.awt.event.ActionListener() {
@@ -107,9 +106,9 @@ public class EscolhaJogadorTela extends javax.swing.JFrame {
         unidade1EnterBotao2.setForeground(new java.awt.Color(255, 255, 255));
         unidade1EnterBotao2.setText("Selecione o Jogador");
 
-        jLabel1.setIcon(new javax.swing.ImageIcon("C:\\Users\\danie\\OneDrive - Colégio Santa Cruz\\Documentos\\NetBeansProjects\\ProjetoIntegradorS1\\src\\main\\java\\com\\mycompany\\mavenproject1\\Imagens\\Silhueta.png")); // NOI18N
+        jLabel1.setIcon(new javax.swing.ImageIcon("C:\\Users\\Diogo\\Documents\\NetBeansProjects\\ProjetoIntegradorS1\\src\\main\\java\\com\\mycompany\\mavenproject1\\Imagens\\Silhueta.png")); // NOI18N
 
-        jLabel4.setIcon(new javax.swing.ImageIcon("C:\\Users\\danie\\OneDrive - Colégio Santa Cruz\\Documentos\\NetBeansProjects\\ProjetoIntegradorS1\\src\\main\\java\\com\\mycompany\\mavenproject1\\Imagens\\Guerra.png")); // NOI18N
+        jLabel4.setIcon(new javax.swing.ImageIcon("C:\\Users\\Diogo\\Documents\\NetBeansProjects\\ProjetoIntegradorS1\\src\\main\\java\\com\\mycompany\\mavenproject1\\Imagens\\Guerra.png")); // NOI18N
 
         jButton4.setBackground(new java.awt.Color(85, 51, 141));
         jButton4.setFont(new java.awt.Font("Montserrat ExtraBold", 0, 18)); // NOI18N
@@ -121,14 +120,13 @@ public class EscolhaJogadorTela extends javax.swing.JFrame {
         jButton1.setBackground(new java.awt.Color(85, 51, 141));
         jButton1.setFont(new java.awt.Font("Montserrat ExtraBold", 0, 18)); // NOI18N
         jButton1.setForeground(new java.awt.Color(255, 255, 255));
-        jButton1.setText("APAGAR");
+        jButton1.setText("JOGAR");
         jButton1.setBorder(null);
-
-        jButton3.setBackground(new java.awt.Color(85, 51, 141));
-        jButton3.setFont(new java.awt.Font("Montserrat ExtraBold", 0, 18)); // NOI18N
-        jButton3.setForeground(new java.awt.Color(255, 255, 255));
-        jButton3.setText("JOGAR");
-        jButton3.setBorder(null);
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
         jPanel3.setLayout(jPanel3Layout);
@@ -153,16 +151,12 @@ public class EscolhaJogadorTela extends javax.swing.JFrame {
                 .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(jButton4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, 109, Short.MAX_VALUE))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 262, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(347, 347, 347))
+                .addContainerGap(49, Short.MAX_VALUE))
         );
         jPanel3Layout.setVerticalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel3Layout.createSequentialGroup()
-                .addGap(50, 50, 50)
+                .addGap(85, 85, 85)
                 .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addGroup(jPanel3Layout.createSequentialGroup()
                         .addComponent(unidade1EnterBotao1, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -180,12 +174,10 @@ public class EscolhaJogadorTela extends javax.swing.JFrame {
                         .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(jComboBoxNomesJogadores, javax.swing.GroupLayout.DEFAULT_SIZE, 54, Short.MAX_VALUE))))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 56, Short.MAX_VALUE)
-                .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(50, 50, 50))
+                .addContainerGap(125, Short.MAX_VALUE))
         );
 
-        jButton2.setIcon(new javax.swing.ImageIcon("C:\\Users\\danie\\OneDrive - Colégio Santa Cruz\\Documentos\\NetBeansProjects\\ProjetoIntegradorS1\\src\\main\\java\\com\\mycompany\\mavenproject1\\Imagens\\MenuIcon.png")); // NOI18N
+        jButton2.setIcon(new javax.swing.ImageIcon("C:\\Users\\Diogo\\Documents\\NetBeansProjects\\ProjetoIntegradorS1\\src\\main\\java\\com\\mycompany\\mavenproject1\\Imagens\\closeIcon.png")); // NOI18N
         jButton2.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
         jButton2.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
@@ -255,6 +247,10 @@ public class EscolhaJogadorTela extends javax.swing.JFrame {
         System.exit(0);
     }//GEN-LAST:event_jButton2MouseClicked
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
+
     /** 
      * @param args the command line arguments
      */
@@ -294,7 +290,6 @@ public class EscolhaJogadorTela extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
     private javax.swing.JButton jButton4;
     private javax.swing.JComboBox<String> jComboBoxNomesJogadores;
     private javax.swing.JLabel jLabel1;
